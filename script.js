41d29d6a3cae06b9cad2442939f3027f7cb35f53
shubhankarsingh030 <113350090+shubhankarsingh030@users.noreply.github.com>
2023-12-09 18:28:01 +0530

    Backend Views

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
 
+app.use(function(req,res,next){
+    next();
+});
+
 app.get('/', function(req,res){
     res.send("Hello World ab sheek raha hu mei or nodemon bhi sheek chuka hu jo ki chlr nhi raha tha")
 });
 
 app.get('/profile', function(req,res){
     res.send("this is another route called profile")
+});
+
+app.get('/profile/:username', function(req,res){
+    res.send(`this is another route from ${req.params.username}`)
 });
 
 app.listen(3000);
